7c745625328d1bb7ed7c01bfeb95d72d83d04824
Pbrhaasn2002 <pbrhaasn2002@gmail.com>
2025-08-25 22:28:00 +0530

    New

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,6 +7,8 @@ import { services } from "../constants";
 import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 
+// Code
+
 const ServiceCard = ({ index, title, icon }) => (
   <Tilt className='xs:w-[250px] w-full'>
     <motion.div
